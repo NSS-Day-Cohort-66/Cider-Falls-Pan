17e4bdc9c2ea2c7f1863fcf61a5b5de0602aee48
Aedin Pittman <aedinpittman@gmail.com>
2023-08-03 15:40:17 -0500

    Imported guest getter function, added title event listenter

--- scripts/destinations.js
+++ scripts/destinations.js
@@ -1,1 +1,19 @@
-import { getAreas, getServices, getAreaServices } from "./database.js";
+import { getAreas, getServices, getAreaServices, getGuests } from "./database.js";
+
+const guests = getGuests()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const clickTarget = clickEvent.target
+        if (clickTarget.dataset.type === "title") {
+            let count = 0
+            for (const guest of guests) {
+                if (guest.areaId === parseInt(clickTarget.dataset.id)) {
+                    count++
+                }
+            }
+            window.alert(`There are ${output} guests in this area.`)
+        }
+    } 
+)
